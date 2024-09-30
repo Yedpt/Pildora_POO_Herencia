d1512ca8a107f8e9d4ec7158b40b26f1a65b397b
Yeder <ypimentel.tapia@gmail.com>
2024-09-30 22:37:03 +0200

    my first commit

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-// aqui ira mi codigo 
+class Persona {
+    nombre: string;
+    edad: number;
+  
+    constructor(nombre: string, edad: number) {
+      this.nombre = nombre;
+      this.edad = edad;
+    }
+  
+    saludar(): void {
+      console.log(`Hola, soy ${this.nombre} y tengo ${this.edad} años.`);
+    }
+  }
+  
+  class Empleado extends Persona {
+    puesto: string;
+  
+    constructor(nombre: string, edad: number, puesto: string) {
+      super(nombre, edad);
+      this.puesto = puesto;
+    }
+  
+    trabajar(): void {
+      console.log(`${this.nombre} está trabajando como ${this.puesto}.`);
+    }
+  }
+  
+  const empleado1 = new Empleado('María', 28, 'Desarrolladora');
+  empleado1.saludar();  // Hola, soy María y tengo 28 años.
+  empleado1.trabajar(); // María está trabajando como Desarrolladora.
+  
